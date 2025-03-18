0c9f72821d9ab7bd4d4b000f59682aad5af095b5
Gustavo Martins <kernelgm@gmail.com>
2025-03-18 15:58:15 -0300

    change unoptimized for weather icons

--- src/components/weather/weather-icon.tsx
+++ src/components/weather/weather-icon.tsx
@@ -10,8 +10,9 @@ export function WeatherIcon({ icon, condition }: WeatherIconProps) {
     <Image
       src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
       alt={condition}
-      width={80}  // largura da imagem (ajustável)
-      height={80} // altura da imagem (ajustável)
+      width={80}
+      height={80}
+      unoptimized
       className="invert dark:invert-0 transition-transform duration-500 hover:scale-110"
     />
   )
