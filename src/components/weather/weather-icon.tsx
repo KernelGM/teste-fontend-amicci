953337b6e9247b693a2bd998455ffa50ad9e8bd8
Gustavo Martins <kernelgm@gmail.com>
2025-03-18 15:28:22 -0300

    change img html to Image from `Nextjs`

--- src/components/weather/weather-icon.tsx
+++ src/components/weather/weather-icon.tsx
@@ -1,16 +1,18 @@
+import Image from 'next/image'
+
 interface WeatherIconProps {
-    icon: string
-    condition: string
-  }
-  
-  export function WeatherIcon({ icon, condition }: WeatherIconProps) {
-    return (
-      <img
-        src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
-        alt={condition}
-        className="w-20 h-20 invert dark:invert-0 transition-transform duration-500 hover:scale-110"
-      />
-    )
-  }
-  
-  
+  icon: string
+  condition: string
+}
+
+export function WeatherIcon({ icon, condition }: WeatherIconProps) {
+  return (
+    <Image
+      src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+      alt={condition}
+      width={80}  // largura da imagem (ajustável)
+      height={80} // altura da imagem (ajustável)
+      className="invert dark:invert-0 transition-transform duration-500 hover:scale-110"
+    />
+  )
+}
